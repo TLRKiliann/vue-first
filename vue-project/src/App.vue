4e9d1ab417cb528d24737f76a1e27949a09913d9
TLRKiliann <philogenie@protonmail.com>
2023-06-08 14:12:17 +0200

    some changes

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -24,7 +24,20 @@ export default defineComponent({
       testColor: 'testColor',
       textStyle: {color: "orange"},
       isPromoted: 'isPromoted',
-      isNotPromoted: 'isNotPromoted'
+      isNotPromoted: 'isNotPromoted',
+      showElement: true,
+      actors: [
+        {
+          id: 1,
+          name: "Clark Kent",
+          movies: ['Superman', '21 JumpStreet']
+        },
+        {
+          id: 2,
+          name: "Bruce Willis",
+          movies: ['Hostage', '6th sens']
+        },
+      ]
     }
   },
   methods: {
@@ -57,12 +70,15 @@ export default defineComponent({
     <h2 v-bind:style="textStyle">Un autre style</h2>
 
     <h2 v-bind:class="count > 0 ? 'isPromoted' : 'isNotPromoted'">{{ count }}</h2>
+    <h2 v-if="count === 0">Count is equal to Zero</h2>
+    <h2 v-else-if="count < 0" v-bind:class="isNotPromoted">Count is negative</h2>
+    <h2 v-else-if="count > 0" v-bind:class="isPromoted">Count is positive</h2>
 
     <button @click="count++">Increment {{ count }}</button>
     <button @click="decrement">Decrement {{ count }}</button>
 
     <h2>{{ myNum }}</h2>
-    <button @click="add(3)">Add</button>
+    <button @click="add(myNum)">Add</button>
 
     <h2 v-text="myList[1]"></h2>
 
@@ -70,13 +86,25 @@ export default defineComponent({
       <div v-if="item.name === 'Sabine'">Name: {{ item.name }} - Age: {{ item.age }} years</div>
     </div>
     
+    <div v-for="actor in actors" :key="actor.id">
+      {{ actor.movies.toString() }}
+    </div>
+
+    <h2 v-show="showElement">v-show</h2>
+
   </header>
 </template>
 
 <style scoped>
   header {
     line-height: 1.5;
+    width: 350px;
     max-height: 100vh;
+    margin: 0 50%;
+    border: 1px solid slategrey;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px cyan;
+    padding: 10px 20px;
   }
   .testColor {
     color: lightgreen;
